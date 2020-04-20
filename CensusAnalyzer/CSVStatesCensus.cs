1c9306b86eb09b50587a4b213eee3af2aa802fa2
ruchika <ruchika6698@gmail.com>
2020-04-20 15:46:51 +0530

    Added:TC2.5 statecode csv Header incorrect then throw exception

--- CensusAnalyzer/CSVStatesCensus.cs
+++ CensusAnalyzer/CSVStatesCensus.cs
@@ -8,31 +8,43 @@ namespace CensusAnalyzer
 {
     public class CSVStatesCensus
     {
-        private static char delimiter;
-
-        public static object getDataFromCSVFile(string statecode)
+        public static int getDataFromCSVFile(string statecode, char delimiter = ',', string header = "SrNo,State,TIN,StateCode")
         {
+            int count = 0;
             try
             {
-                if (Path.GetExtension(statecode) != ".csv")
+                if (Path.GetExtension(statecode) == ".csv")
                 {
-                    throw new CustomException("File_format_Incorrect");
-                }
-                if (statecode != @"C:\Users\boss\source\repos\CensusAnalyzerProblem\CensusData\StateCensus.csv")
-                {
-                    throw new CustomException("File_not_found");
-                }
-                string[] data = File.ReadAllLines(statecode);
-                //check delimiter is correct or incorrect
-                foreach (string str in data)
-                {
-
-                    if (str.Split(delimiter).Length != 4 && str.Split(delimiter).Length != 2)
+                    string[] data = File.ReadAllLines(statecode);
+                    //check delimiter is correct or incorrect
+                    foreach (string str in data)
                     {
-                        throw new CustomException("Incorrect Delimiter");
+
+                        if (str.Split(delimiter).Length != 4 && str.Split(delimiter).Length != 2)
+                        {
+                            throw new CustomException("Incorrect Delimiter");
+                        }
                     }
+                    //checking Incorrect header
+                    if (!data[0].Equals(header))
+                    {
+                        throw new CustomException("Incorrect header");
+                    }
+                    IEnumerable<string> ele = data;
+                    foreach (var elements in ele)
+                    {
+                        count++;
+                    }
+                    return count;
                 }
-                return data.Length;
+                else                                                   //if file type incorrect then throw exception
+                {
+                    throw new CustomException("File type incorrect");
+                }
+            }
+            catch (FileNotFoundException)                              //if file path incorrect then throw exception
+            {
+                throw new CustomException("file path incorrect");
             }
             catch (CustomException)
             {
@@ -40,5 +52,4 @@ namespace CensusAnalyzer
             }
         }
     }
-
 }
